12578c425a0654fbdd7ff9d35bb85ee4b95d1297
elenagolovko <airdragon220@gmail.com>
2018-09-23 11:23:36 +0300

    layout change, working on creating address

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/styles.scss';
+
+('use strict');
+
+(function() {
+  let form = document.querySelector('.new-note__form');
+
+  form.addEventListener('submit', evt => {
+    evt.preventDefault();
+    // if (checkValidity(value)) {
+    // window.backend.save(new FormData(form), function () {
+    //   при удачном отправлении
+    // }, function () {
+    //   document.querySelector('.input__error').classList.remove('visually-hidden');
+    // });
+    // }
+  });
+})();
